351f61871839c2e024ac31a650d8e3bafe7ba74f
Stefan Scherer <scherer_stefan@icloud.com>
2016-09-22 17:53:57 +0200

    Fix all tests

--- spec/windows_spec.rb
+++ spec/windows_spec.rb
@@ -4,22 +4,20 @@ set :backend, :cmd
 set :os, :family => 'windows'
 
 describe "Some package tests" do
-  # AppVeyor Build Agent
-  describe package("mypackage") do
+  describe package("AppVeyor Build Agent") do
     it { should be_installed}
   end
 
-  # DHCP Client
-  describe service("mypackage") do
+  describe service("DHCP Client") do
     it { should be_running }
     it { should have_start_mode("Automatic") }
   end
 
-  describe file("#{ENV['ProgramFiles']}/mypackage/git-bash.exe") do
+  describe file("#{ENV['ProgramFiles']}/git/git-bash.exe") do
     it { should exist }
   end
 
-  describe file("#{ENV['ProgramFiles']}/mypackage/") do
+  describe file("#{ENV['ProgramFiles']}/git /") do
     it { should be_directory }
   end
 end
