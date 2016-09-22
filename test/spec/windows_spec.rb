180829a32088cb4a165c4914b06114aa5b94cabe
Stefan Scherer <scherer_stefan@icloud.com>
2016-09-22 17:41:05 +0200

    Update Gemfile

--- test/spec/windows_spec.rb
+++ test/spec/windows_spec.rb
@@ -4,20 +4,22 @@ set :backend, :cmd
 set :os, :family => 'windows'
 
 describe "Some package tests" do
+  # AppVeyor Build Agent
   describe package("mypackage") do
     it { should be_installed}
   end
 
+  # DHCP Client
   describe service("mypackage") do
     it { should be_running }
     it { should have_start_mode("Automatic") }
   end
 
-  describe file("#{ENV['ProgramFiles']}/a/file") do
+  describe file("#{ENV['ProgramFiles']}/mypackage/git-bash.exe") do
     it { should exist }
   end
 
-  describe file("#{ENV['ProgramFiles']}/a/directory/") do
+  describe file("#{ENV['ProgramFiles']}/mypackage/") do
     it { should be_directory }
   end
 end
